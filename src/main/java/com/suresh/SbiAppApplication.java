f0361e548b413c9e0daee849cbc5edae6b545b35
suresh <asureshdora8@gmail.com>
2020-08-26 19:03:34 +0530

    Update main

--- src/main/java/com/suresh/SbiAppApplication.java
+++ src/main/java/com/suresh/SbiAppApplication.java
@@ -2,12 +2,13 @@ package com.suresh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SbiAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SbiAppApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(SbiAppApplication.class, args);
 	}
 
 }
