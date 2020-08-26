735e34b721467f63cd50d091ca834ca27eff4df8
suresh <asureshdora8@gmail.com>
2020-08-26 18:52:40 +0530

    SBI App Project

---
+++ src/test/java/com/suresh/SbiAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suresh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbiAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
